249de52b367cb0e5bc9adce7ac1e9a88fc579692
Naoki Seto <lionas.watanabe@gmail.com>
2017-08-24 14:33:48 +0900

    エラー処理の追加と出力フォーマット調整 (#2)

--- EmojiExtractor/main.swift
+++ EmojiExtractor/main.swift
@@ -8,14 +8,25 @@
 
 import Foundation
 
-let emojiDataFileUrl = "file://" + CommandLine.arguments[1]
+if CommandLine.argc < 2 {
+    // 引数エラー
+    print("絵文字定義ファイルが指定されていません.引数に絵文字定義ファイルの絶対パスを指定してください.")
+} else {
+    
+    // 絵文字ファイルのURL
+    let emojiDataFileUrl = "file://" + CommandLine.arguments[1]
+    
+    // 絵文字ファイルの読み込み
+    let emojiData = readEmojiDataFile(emojiDataPath: emojiDataFileUrl)
+    
+    // 文字コードの取り出し＆正規表現パターンの生成
+    // [\\x{9999}\\x{9999}...
+    let emojiCodePattern = extractEmoji(text: emojiData)
+    
+    // 結果出力
+    print("絵文字検索用正規表現パターン>>>")
+    print(emojiCodePattern)
+    
+}
 
-// 絵文字データファイルの読み込み
-let emojiData = readEmojiDataFile(emojiDataPath: emojiDataFileUrl)
-
-// 文字コードの取り出し＆正規表現パターンの生成
-// [\\x{9999}\\x{9999}...
-let emojiCodePattern = extractEmoji(text: emojiData)
-
-print(emojiCodePattern)
 
